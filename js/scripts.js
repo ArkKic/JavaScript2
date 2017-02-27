ae419b02e8304c6b0937f6b562dafa3b91f895ad
arekk <arkadiuszkicior@gmail.com>
2017-02-27 22:45:14 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*
+  komentarz blokowy
+  console.log(‘Zakomentowana się nie wywołam’);
+*/
+
+var name = prompt('Witaj na mojej stronie - podaj swoje imię');
+/*
+  komentarz blokowy
+  na potrzeby tego zadania
+  do zakomentowania alert
+*/
+
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
